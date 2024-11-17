4d8ff3c64d9eb9d943442780252c148650e5e639
Kreap-0 <1493025761@qq.com>
2024-11-17 18:07:58 +0800

    Fix few bugs

--- include/Executor.hpp
+++ include/Executor.hpp
@@ -11,7 +11,7 @@ struct Pose {
 class Executor
 {
 public:
-    static Executor* NewExecutor(const Pose& pose = {0, 0, 'N'});
+    static Executor* NewExecutor(const Pose& pose) noexcept;
 
     Executor(void) = default;
     virtual ~Executor(void) = default;
