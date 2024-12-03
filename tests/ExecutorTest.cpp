c9d52a0d5fd5a4bec14935490769af0cd1688185
Kreap-0 <1493025761@qq.com>
2024-12-03 18:00:52 +0800

    Add test cases for reverse command

--- tests/ExecutorTest.cpp
+++ tests/ExecutorTest.cpp
@@ -204,7 +204,128 @@ TEST(ExecutorFastTest, should_return_S_and_x_plus_1_given_status_is_fast_given_c
 TEST(ExecutorFastTest, should_return_y_plus_1_given_command_is_FFM_and_facing_is_N)
 {
     // given
-    std::unique_ptr<Executor> executor(Executor::NewExecutor());
+    std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'N'}));
+    // when
+    executor->Execute("FFM");
+    // then
+    const Pose target{0, 1, 'N'};
+    ASSERT_EQ(target, executor->Query());
+}
+
+TEST(ExecutorReverseTest, should_return_x_minus_1_given_command_is_BM_and_facing_is_E)
+{
+    // given
+    std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
+    // when
+    executor->Execute("BM");
+    // then
+    const Pose target{-1, 0, 'E'};
+    ASSERT_EQ(target, executor->Query());
+}
+
+TEST(ExecutorReverseTest, should_return_S_given_command_is_BL_and_facing_is_E)
+{
+    // given
+    std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
+    // when
+    executor->Execute("BL");
+    // then
+    const Pose target{0, 0, 'S'};
+    ASSERT_EQ(target, executor->Query());
+}
+
+TEST(ExecutorReverseTest, should_return_N_given_command_is_BR_and_facing_is_E)
+{
+    // given
+    std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
+    // when
+    executor->Execute("BR");
+    // then
+    const Pose target{0, 0, 'N'};
+    ASSERT_EQ(target, executor->Query());
+}
+
+TEST(ExecutorReverseTest, should_return_x_plus_2_given_command_is_FM_and_facing_is_E)
+{
+    // given
+    std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
+    // when
+    executor->Execute("FM");
+    // then
+    const Pose target{2, 0, 'E'};
+    ASSERT_EQ(target, executor->Query());
+}
+
+TEST(ExecutorReverseTest, should_return_N_and_x_plus_1_given_command_is_FL_and_facing_is_E)
+{
+    // given
+    std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
+    // when
+    executor->Execute("FL");
+    // then
+    const Pose target{1, 0, 'N'};
+    ASSERT_EQ(target, executor->Query());
+}
+
+TEST(ExecutorReverseTest, should_return_S_and_x_plus_1_given_command_is_FR_and_facing_is_E)
+{
+    // given
+    std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
+    // when
+    executor->Execute("FR");
+    // then
+    const Pose target{1, 0, 'S'};
+    ASSERT_EQ(target, executor->Query());
+}
+
+TEST(ExecutorReverseTest, should_return_x_minus_2_given_command_is_BFM_and_facing_is_E)
+{
+    // given
+    std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
+    // when
+    executor->Execute("BFM");
+    // then
+    const Pose target{-2, 0, 'E'};
+    ASSERT_EQ(target, executor->Query());
+}
+
+TEST(ExecutorReverseTest, should_return_S_and_x_minus_1_given_command_is_BFL_and_facing_is_E)
+{
+    // given
+    std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
+    // when
+    executor->Execute("BFL");
+    // then
+    const Pose target{-1, 0, 'S'};
+    ASSERT_EQ(target, executor->Query());
+}
+
+TEST(ExecutorReverseTest, should_return_N_and_x_minus_1_given_command_is_BFR_and_facing_is_E)
+{
+    // given
+    std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
+    // when
+    executor->Execute("BFR");
+    // then
+    const Pose target{-1, 0, 'N'};
+    ASSERT_EQ(target, executor->Query());
+}
+
+TEST(ExecutorReverseTest, should_return_y_plus_1_given_command_is_BBM_and_facing_is_N)
+{
+    // given
+    std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'N'}));
+    // when
+    executor->Execute("BBM");
+    // then
+    const Pose target{0, 1, 'N'};
+    ASSERT_EQ(target, executor->Query());
+}
+
+TEST(ExecutorReverseTest, should_return_y_plus_1_given_command_is_FFM_and_facing_is_N)
+{
+    // given
+    std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'N'}));
     // when
     executor->Execute("FFM");
     // then
