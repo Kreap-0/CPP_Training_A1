d80d6d3dcf3ae8d43cd9068e8162f610484ab900
Kreap-0 <1493025761@qq.com>
2024-12-03 17:20:18 +0800

    Point class encapsulation

---
+++ src/Point.cpp
@@ -0,0 +1,26 @@
+#include "Point.hpp"
+
+namespace adas
+{
+
+Point::Point(const int x, const int y) noexcept : x(x), y(y)
+{
+}
+
+Point& Point::operator+=(const Point& rhs) noexcept
+{
+    x += rhs.GetX();
+    y += rhs.GetY();
+}
+
+int Point::GetX() const noexcept
+{
+    return x;
+}
+
+int Point::GetY() const noexcept
+{
+    return y;
+}
+
+}  // namespace adas
